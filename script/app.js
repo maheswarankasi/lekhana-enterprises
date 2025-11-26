a0d743c5ddce9c073d47d1a90661f7315393d3ff
maheswarankasi <maheskasi007@gmail.com>
2025-11-26 17:57:34 +0530

    gallery partially completed

--- script/app.js
+++ script/app.js
@@ -1,3 +1,16 @@
-$(function () {
-  "use strict";
+$(document).ready(function(){
+  // Show button when scrolled down 100px
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('#backToTop').fadeIn();
+    } else {
+      $('#backToTop').fadeOut();
+    }
+  });
+
+  // Scroll smoothly to top when clicked
+  $('#backToTop').click(function(){
+    $('html, body').animate({scrollTop : 0}, 600);
+    return false;
+  });
 });
